65fcb760c96daf3ebcb93fbd8677c80c3460edff
earkel12 <earkel@naver.com>
2025-06-18 10:40:46 +0900

    test

---
+++ ParkingMate/src/main/java/com/pm/ParkingMateApplication.java
@@ -0,0 +1,13 @@
+package com.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingMateApplication.class, args);
+	}
+
+}
